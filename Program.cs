d77f6834585492e981ce28e121a80def0c7c6bc6
Ashfana Ashfu <shaikhashfana9@gmail.com>
2021-11-13 20:18:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NPrime_num
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("enter a number");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The first N prime numbers are:");
+            int noOfPrimeCount = 0, count, num = 2;
+            while (noOfPrimeCount < n)
+            {
+                count = 0;
+                for (int i = 2; i <= num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        count++;
+                    }
+                    if (count >= 2)
+                    {
+                        break;
+                    }
+                }
+                if (count < 2)
+                {
+                    noOfPrimeCount++;
+                    Console.WriteLine(num);
+                }
+                num++;
+            }
+        }
+    }
+    }
+
